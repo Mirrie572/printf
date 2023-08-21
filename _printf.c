827aaa6a1a4f21b46c7b744cf36aa5ad30a27ed1
Mirriam Maggiefwamba Mwawa <maggie05072000@gmail.com>
2023-08-21 02:30:44 -0700

    a function that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,56 @@
+/**
+ * _printf - Custom implementation of printf function
+ * @format: Format string
+ * @...: Additional arguments
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+	char c, *s;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format++, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				s = va_arg(args, char *);
+
+				while (*s)
+				{
+					write(1, s++, 1);
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				write(1, format++, 1);
+				count++;
+			}
+		}
+	}
+
+	va_end(args);
+
+	return (count);
+}
